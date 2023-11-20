ff4af46e877ab646e48b7e3b0e1355980b71864f
abhaynm02 <abhaybhaskarnm02@gmail.com>
2023-11-20 13:03:15 +0530

    first commit

---
+++ furni/src/test/java/com/firstProject/furni/FurniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstProject.furni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
